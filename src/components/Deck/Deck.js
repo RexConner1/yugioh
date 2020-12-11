27efafbaabeeabc78e371f41df5dd899bc8b88f2
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-11 11:28:07 -0600

    Deck image

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
 import './Deck.css';
-// import Card from '../Card/Card'
+import Card from '../Card/Card'
 
 class Deck extends Component {
     constructor() {
         super();
         this.state = {
-
+            image: '/deck.png'
         }
     }
 
     render() {
         return (
           <div>
-
+              <Card image={this.state.image} />
           </div>
         );
     }
