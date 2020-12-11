2556b059351a65acaacfefc1eae418aa1a29386f
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-11 15:02:00 -0600

    Player beginning methods

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 
 import './Player.css';
 import Board from '../Board/Board';
+// import Hand from '../Hand/Hand';
 
 class Player extends Component {
     constructor() {
         super();
         this.state = {
-            lifePoints: 8000
+            deck: [],
+            lifePoints: 8000,
         }
+    }
+
+    componentDidMount = () => {
+        this.getDeck()
+    }
+
+    getDeck = async() => {
+        
     }
 
     render() {
         return (
           <div>
               <Board draw={this.props.draw} />
+              {/* <Hand /> */}
           </div>
         );
     }
