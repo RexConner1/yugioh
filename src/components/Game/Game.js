1c3bf43af166c0c9ae9a5b39ec53ea98f4a68b5b
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-10 22:44:36 -0600

    Create Game class

---
+++ src/components/Game/Game.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './Game.css';
+
+class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+          <div>
+              <h1>Game</h1>
+          </div>
+        );
+    }
+}
+
+export default Game;
