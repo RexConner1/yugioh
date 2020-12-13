af941c30f422d95538d5f977b96194a3f29a2680
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-12 23:55:57 -0600

    Sample hand

--- src/components/Hand/Hand.js
+++ src/components/Hand/Hand.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 
 import './Hand.css';
-// import Card from '../Card/Card'
+import Card from '../Card/Card'
 
 class Hand extends Component {
     constructor() {
         super();
         this.state = {
-
+            zoneImage: "https://storage.googleapis.com/ygoprodeck.com/pics/11714098.jpg"
         }
     }
 
     render() {
         return (
-          <div>
-              
+          <div className="hand">
+              <div className="card">
+                <Card image={this.state.zoneImage} />
+              </div>
+              <div className="card">
+                <Card image={this.state.zoneImage} />
+              </div>
+              <div className="card">
+                <Card image={this.state.zoneImage} />
+              </div>
+              <div className="card">
+                <Card image={this.state.zoneImage} />
+              </div>
+              <div className="card">
+                <Card image={this.state.zoneImage} />
+              </div>
           </div>
         );
     }
