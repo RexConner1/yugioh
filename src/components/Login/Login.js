9752e61b34d82fc2cb6206059776cfbcb6013a80
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-14 15:33:49 -0600

    Use login example

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import './Login.css';
+axios.defaults.baseURL = 'http://localhost:3000/api'
 
-class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState();
 
-        }
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
     }
+  }, []);
 
-    render() {
-        return (
-          <div className="login">
-              <form onSubmit={(e) => {
-                  e.preventDefault()
-                  console.log(e.target.username.value)
-                  console.log(e.target.password.value)
-              }}>
-                      <label htmlFor="username">User Name: </label>
-                      <input name="username" id="username" />
-                      <br/>
-                      <label htmlFor="password">Password: </label>
-                      <input name="password" id="password" />
-                      <br />
-                      <button>Submit</button>
-              </form>
-          </div>
-        );
-    }
-}
+  // logout the user
+  const handleLogout = () => {
+    setUser({});
+    setUsername("");
+    setPassword("");
+    localStorage.clear();
+  };
+
+  // login the user
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const user = { username, password };
+    // send the username and password to the server
+    const response = await axios.post(
+      "/auth/login",
+      user
+    );
+    // set the state of the user
+    setUser(response.data);
+    // store the user in localStorage
+    localStorage.setItem("user", JSON.stringify(response.data));
+  };
+
+  // if there's a user show the message below
+  if (user) {
+    return (
+      <div>
+        {user.name} is loggged in
+        <button onClick={handleLogout}>logout</button>
+      </div>
+    );
+  }
+
+  // if there's no user, show the login form
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter a username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter a password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
