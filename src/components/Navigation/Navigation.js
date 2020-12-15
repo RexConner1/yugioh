4b0d9f9625996c61cc669aa9e68bbcf48567be89
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-15 12:40:50 -0600

    Avoid resets

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,11 +6,12 @@ class Navigation extends Component {
     render() {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="/">Yu-Gi-Oh</Navbar.Brand>
+                <Navbar.Brand><Link to="/">Yu-Gi-Oh</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/game">New Game</Nav.Link>
+                        {/* <Nav.Link href="/game">New Game</Nav.Link> */}
+                        <Nav.Link><Link to="/game">New Game</Link></Nav.Link>
                     </Nav>
                     <Nav>
                         {/* <Nav.Link href={this.props.loggedIn ? '/decks/edit' : '/signup'}>{this.props.loggedIn ? 'Edit Deck' : 'Sign Up'}</Nav.Link> */}
