5bd6e5842edb4e0ffa0049f416d853aaac9dba59
David Orona <OronaDavidJ@JohnDeere.com>
2020-12-14 21:00:30 -0600

    More info

--- src/components/EditDecks/EditDecks.js
+++ src/components/EditDecks/EditDecks.js
@@ -16,6 +16,24 @@ class EditDecks extends Component {
                     "userId": 1,
                     "createdAt": "2020-12-13T02:54:09.232Z",
                     "updatedAt": "2020-12-13T02:54:09.232Z",
+                    "Cards": [
+                        {
+                            "id": 1,
+                            "cardNumber": 11714098,
+                            "name": "30,000-Year White Turtle",
+                            "Stat": {
+                                "level": 5,
+                                "attack": 1250,
+                                "defense": 2100
+                            },
+                            "DeckCard": {
+                                "deckId": 3,
+                                "cardId": 1,
+                                "createdAt": "2020-12-13T02:54:09.238Z",
+                                "updatedAt": "2020-12-13T02:54:09.238Z"
+                            }
+                        }
+                    ]
                 },
                 {
                     "id": 2,
@@ -23,6 +41,24 @@ class EditDecks extends Component {
                     "userId": 1,
                     "createdAt": "2020-12-13T02:54:09.232Z",
                     "updatedAt": "2020-12-13T02:54:09.232Z",
+                    "Cards": [
+                        {
+                            "id": 1,
+                            "cardNumber": 11714098,
+                            "name": "30,000-Year White Turtle",
+                            "Stat": {
+                                "level": 5,
+                                "attack": 1250,
+                                "defense": 2100
+                            },
+                            "DeckCard": {
+                                "deckId": 2,
+                                "cardId": 1,
+                                "createdAt": "2020-12-13T02:54:09.238Z",
+                                "updatedAt": "2020-12-13T02:54:09.238Z"
+                            }
+                        }
+                    ]
                 },
                 {
                     "id": 1,
@@ -30,6 +66,56 @@ class EditDecks extends Component {
                     "userId": 1,
                     "createdAt": "2020-12-13T02:54:09.232Z",
                     "updatedAt": "2020-12-13T02:54:09.232Z",
+                    "Cards": [
+                        {
+                            "id": 1,
+                            "cardNumber": 11714098,
+                            "name": "30,000-Year White Turtle",
+                            "Stat": {
+                                "level": 5,
+                                "attack": 1250,
+                                "defense": 2100
+                            },
+                            "DeckCard": {
+                                "deckId": 1,
+                                "cardId": 1,
+                                "createdAt": "2020-12-13T02:54:09.238Z",
+                                "updatedAt": "2020-12-13T02:54:09.238Z"
+                            }
+                        },
+                        {
+                            "id": 5,
+                            "cardNumber": 47372349,
+                            "name": "Acrobat Monkey",
+                            "Stat": {
+                                "level": 3,
+                                "attack": 1000,
+                                "defense": 1800
+                            },
+                            "DeckCard": {
+                                "deckId": 1,
+                                "cardId": 5,
+                                "createdAt": "2020-12-14T17:09:31.285Z",
+                                "updatedAt": "2020-12-14T17:09:31.285Z"
+                            }
+                        },
+                        {
+                            "id": 10,
+                            "cardNumber": 43096270,
+                            "name": "Alexandrite Dragon",
+                            "Stat": {
+                                "level": 4,
+                                "attack": 2000,
+                                "defense": 100
+                            },
+                            "DeckCard": {
+                                "deckId": 1,
+                                "cardId": 10,
+                                "createdAt": "2020-12-14T17:10:39.006Z",
+                                "updatedAt": "2020-12-14T17:10:39.006Z"
+                            }
+                        }
+                    ]
                 }
             ]
         }
@@ -64,10 +150,10 @@ class EditDecks extends Component {
     render() {
         const decks = this.state.decks.map(deck => {
             return (
-                <tr key={deck.id}>
+                <tr key={deck.id} onClick={() => console.log('Test')}>
                     <th scope="row">{deck.id}</th>
-                    <td>{deck.name}</td>
-                    <td>{deck.userId}</td>
+                    <td onDoubleClick={() => console.log('Hi')}>{deck.name}</td>
+                    <td>{deck.Cards.length}</td>
                     <td>{deck.createdAt}</td>
                     <td>{deck.updatedAt}</td>
                     <td><a href="/edit">Edit</a></td>
